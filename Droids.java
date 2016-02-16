8142795ba6534cce1461086ad3fe5226a8872e0a
AbsoluteZer0-II <cameronvardaman304@hotmail.com>
2016-02-16 13:02:30 -0700

    Update Droids.java

--- Droids.java
+++ Droids.java
@@ -7,7 +7,7 @@ public class Droid
     
     public boolean Trade(int i)
     {
-        if(resource < (3 * i.resource))
+        if(resource < (3 * i.resource))//Names of resource will be changed according to resource card class
             return true;
         else
             return false;
