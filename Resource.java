9d25fa434d79d2a805bfd86987be7820b4bcf7b8
dankmemes666 <rburdett4@gmail.com>
2016-02-16 13:08:04 -0700

    Create Resource.java

---
+++ Resource.java
@@ -0,0 +1,20 @@
+public class Resource
+{
+    public int wood = 19;
+    public int brick = 19;
+    public int ore = 19;
+    public int sheep =19;
+    public int wheat = 19;
+    public static void main(String[] args)
+    {
+        
+    }
+    public Resource()
+    {
+        
+    }
+    public void drawCard()
+    {
+        
+    }
+}
