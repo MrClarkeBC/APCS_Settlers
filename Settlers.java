7dda0de0f74b352ce999b69bfd96b1065662b426
81698 <allen.m.clarke@gmail.com>
2016-02-16 13:02:28 -0700

    Update Settlers.java

--- Settlers.java
+++ Settlers.java
@@ -7,6 +7,8 @@ public class Settlers
   public void main(String[] args)
   {
     Settlers s1 = new Settlers();
+    Harbors h1 = new Harbors(true);
+    Harbors h2 = new Harbors(false);
   }
   
 }
