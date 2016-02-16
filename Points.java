d2e5c85a0ce93111933a16dfcf97770fd52311f4
scottieperry13 <scottm.perry@yahoo.com>
2016-02-16 13:04:48 -0700

    Update Points.java

--- Points.java
+++ Points.java
@@ -1,6 +1,28 @@
 public class Points{
+    
     int m_points;
+    int m_settlements;
+    int m_cities;
+    boolean m_longroad;
+    boolean m_bigarmy;
+    
     public Points(){
 
+    }
+    
+    public String toString(){
+    
+    }
+    
+    public boolean checkLongRoad(){
+    
+    }
+    
+    public boolean checkBigArmy(){
+    
+    }
+    
+    public int totalPoints(){
+    
     }
 }
