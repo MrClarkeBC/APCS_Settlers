b805e103932ae5a45b78f6ca798edbe8f5afe9eb
We Stand Together <trentsunshine234@gmail.com>
2016-02-16 13:08:30 -0700

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,13 @@
+public class Number
+{
+    int m_num;
+    public Number(int num)
+    {
+        int m_num = num;
+    }
+    
+    public String toString()
+    {
+        return m_num + "";
+    }
+}
