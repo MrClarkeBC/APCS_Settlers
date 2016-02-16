e67a58f2fbcecbcf15f59e4ab088f31866c0e882
81698 <allen.m.clarke@gmail.com>
2016-02-16 13:00:47 -0700

    Create Harbors.java

---
+++ Harbors.java
@@ -0,0 +1,23 @@
+
+public class Harbors
+{
+    boolean m_is2For1;
+    public static void main(String[] args) 
+    {
+    }
+
+    public Harbors(boolean is2For1)
+    {
+        m_is2For1 = is2For1;
+    }
+
+    /*   public Resource tradeIn(Resource give)
+    {
+    (if(m_is2For1)
+    give -= 2;
+    else
+    give -= 3;
+    return new Resource
+    }
+     */
+}
