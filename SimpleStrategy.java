c57bd76c633b265b19da6324bc0d9b1126b736a3
MrClarkeBC <tom.a.clarke@gmail.com>
2016-03-27 10:27:57 -0700

    Added files via upload
    New version with basic AI interface.

---
+++ SimpleStrategy.java
@@ -0,0 +1,27 @@
+public class SimpleStrategy implements PlayerStrategy
+{
+    public SOC.Junction placeFirstSettlement(BoardInterface b)
+    {
+        return b.availableJunctions().get(0);
+    }
+    public SOC.Junction placeFirstRoad(BoardInterface b)
+    {
+        return b.availableRoads().get(0);
+    }
+    public SOC.Junction placeSecondSettlement(BoardInterface b)
+    {
+        return b.availableJunctions().get(0);
+    }
+    public SOC.Road placeSecondRoad(BoardInterface b)
+    {
+        return b.availableRoads().get(0);
+    }
+    public void takeTurn(BoardInterface b)
+    {
+        if (b.availableRoads().size() > 0)
+            b.build(b.availableRoads().get(0));
+        if (b.availableJunctions().size() > 0)
+            b.build(b.availableJunctions().get(0));
+    }
+
+}
