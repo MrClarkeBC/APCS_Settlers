bc31d90191845b0c9517d32e4728ae6bb83930cd
rwheagle <rwh.kyler@gmail.com>
2016-04-04 14:00:53 -0700

    Create Robber.java

---
+++ Robber.java
@@ -0,0 +1,20 @@
+
+public class Robber
+{
+    private int m_location; 
+    public Robber(int loc)
+    {
+        m_location = loc;
+    }
+
+    public int getLocation()
+    {
+        return m_location;
+    }
+
+    public void newLocation(int loc)
+    {
+        m_location = loc;
+    }
+
+}
